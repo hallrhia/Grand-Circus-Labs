3cd5bbd81c736b454e4fcdfcacce9bfa5634990c
Rhianon Hall <hallrhia@gmail.com>
2022-10-13 12:15:14 -0400

    first commit

---
+++ JavaScript-Refresher/script.js
@@ -0,0 +1,15 @@
+let firstName = "Rhianon";
+let lastName = "Hall";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 93018;
+
+console.log(population);
+
+console.log(population *=3);
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
