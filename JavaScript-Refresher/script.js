5a7c55e01f5045e73213cbbd1b30164805bb5d13
Rhianon Hall <hallrhia@gmail.com>
2022-10-13 17:02:13 -0400

    finished

--- JavaScript-Refresher/script.js
+++ JavaScript-Refresher/script.js
@@ -13,3 +13,11 @@ console.log(population *=3);
 let javaScriptIsCool = true;
 
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+for (let i=1; i < 6; i++) {
+    console.log(i);
+}
+
+for (let i=0; i < 5; i++) {
+    console.log(i)
+}
