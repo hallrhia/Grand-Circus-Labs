b7ae8bc942fe699320f6a1b2e6bc1fece3f323c0
Rhianon Hall <hallrhia@gmail.com>
2022-10-16 16:51:59 -0400

    first attempt

---
+++ Heating-Cooling/script.js
@@ -0,0 +1,13 @@
+let actualTemp = 68;
+
+let desiredTemp = 68;
+
+if(actualTemp < desiredTemp) {
+    console.log("Run heat");
+}
+else if (actualTemp > desiredTemp ){
+    console.log("Run A/C");
+}
+else{
+    console.log("Standby");
+}
