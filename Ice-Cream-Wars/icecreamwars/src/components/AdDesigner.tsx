f49049134066aca1cabfc9d8d54dc6a4b3ca9e37
Rhianon Hall <hallrhia@gmail.com>
2023-01-22 17:14:24 -0500

    readyish

--- Ice-Cream-Wars/icecreamwars/src/components/AdDesigner.tsx
+++ Ice-Cream-Wars/icecreamwars/src/components/AdDesigner.tsx
@@ -1,45 +1,51 @@
-import { useState } from 'react';
-import '../css/AdDesigner.css';
-import Ad from './Ad';
-
+import { useState } from "react";
+import "../css/AdDesigner.css";
+import Ad from "./Ad";
 
 function AdDesigner() {
-    const [flavor, setFlavor] = useState<string>('Chocolate');
-    const [isLight, setIsLight] = useState<boolean>(true);
-    const [fontSize, setFontSize] = useState<number>(24);
-    
+  const [flavor, setFlavor] = useState<string>("");
+  const [isLight, setIsLight] = useState<boolean>(true);
+  const [fontSize, setFontSize] = useState<number>(40);
 
-    return(
-        <div id="AdDesign">
-            <h2>Ad Designer</h2>
+  return (
+    <div id="AdDesign">
+      <h2>Ad Designer</h2>
 
-            <Ad flavor={flavor} isLight={isLight} fontSize={fontSize}/>
-            
-            <h3>What to Support</h3>
-            <div className="button-bar">
-            <button onClick={() => setFlavor('Chocolate')}>Chocolate</button>
-            <button onClick={() => setFlavor('Vanilla')}>Vanilla</button>
-            <button onClick={() => setFlavor('Strawberry')}>Strawberry</button>
-            </div>
-            
+      <Ad flavor={flavor} isLight={isLight} fontSize={fontSize} />
 
-            <h3>Color Theme</h3>
-            <div className="button-bar">
-            <button onClick={() => setIsLight(true)}>Light</button>
-            <button onClick={() => setIsLight(false)}>Dark</button>
-            </div>
-            
+      <h3>What to Support</h3>
+      <div className="button-bar">
+        {flavor === "Chocolate" ? (
+          <button onClick={() => setFlavor("Chocolate")} disabled>Chocolate</button>
+        ) : (
+          <button onClick={() => setFlavor("Chocolate")}>Chocolate</button>
+        )}
+        {flavor === "Vanilla" ? (
+          <button onClick={() => setFlavor("Vanilla")} disabled>Vanilla</button>
+        ) : (
+          <button onClick={() => setFlavor("Vanilla")}>Vanilla</button>
+        )}
+        {flavor === "Strawberry" ? (
+          <button onClick={() => setFlavor("Strawberry")} disabled>Strawberry</button>
+        ) : (
+          <button onClick={() => setFlavor("Strawberry")}>Strawberry</button>
+        )}
+      </div>
 
-            <h3>Font Size</h3>
-            <div className="button-bar">
-            <button onClick={() => setFontSize(fontSize - 1)}>Down</button>
-            <input type="number" value={fontSize} />
-            <button onClick={() => setFontSize(fontSize + 1)}>Up</button>
-            </div>
-            
-        </div>
+      <h3>Color Theme</h3>
+      <div className="button-bar">
+        <button onClick={() => setIsLight(true)}>Light</button>
+        <button onClick={() => setIsLight(false)}>Dark</button>
+      </div>
 
-    )
+      <h3>Font Size</h3>
+      <div className="button-bar">
+        <button onClick={() => setFontSize(fontSize - 1)}>Down</button>
+        <input type="number" value={fontSize} />
+        <button onClick={() => setFontSize(fontSize + 1)}>Up</button>
+      </div>
+    </div>
+  );
 }
 
 export default AdDesigner;
--- Ice-Cream-Wars/icecreamwars/src/components/Votes.tsx
+++ Ice-Cream-Wars/icecreamwars/src/components/Votes.tsx
@@ -1,60 +1,69 @@
-import { useState } from 'react';
-import '../css/Votes.css';
+import { useState } from "react";
+import "../css/Votes.css";
 
 function Votes() {
-    const [chocolateVotes, setChocolateVotes] = useState(0);
-    const [vanillaVotes, setVanillaVotes] = useState(0);
-    const [strawberryVotes, setStrawberryVotes] = useState(0);
-    const [visible, setVisible] = useState(true);
+  const [chocolateVotes, setChocolateVotes] = useState(0);
+  const [vanillaVotes, setVanillaVotes] = useState(0);
+  const [strawberryVotes, setStrawberryVotes] = useState(0);
+ 
+  function getTotal() {
+    return chocolateVotes + strawberryVotes + vanillaVotes;
+  }
 
+  function calculatePercentage(votes: number, totalVotes: number) {
+    return ((votes / totalVotes) * 100).toFixed(1) + "%";
+  }
 
-    function getTotal(){
-        return chocolateVotes + strawberryVotes + vanillaVotes;
-    }
-    
-    function calculatePercentage(votes:number, totalVotes:number){
-        return (votes / totalVotes * 100).toFixed(1) + '%';
-    }
-  
-    // function showBar() {
-        
-    // }
+  return (
+    <div className="Votes">
+      <h2>Vote Here</h2>
+      <div className="button-bar">
+        <button onClick={() => setChocolateVotes(chocolateVotes + 1)}>Chocolate</button>
+        <button onClick={() => setVanillaVotes(vanillaVotes + 1)}>Vanilla</button>
+        <button onClick={() => setStrawberryVotes(strawberryVotes + 1)}>Strawberry</button>
+      </div>
 
-    return(
-        <div className="Votes">
-            <h2>Vote Here</h2>
-            <div className="button-bar">
-                <button onClick={()=> {
-                    setChocolateVotes(chocolateVotes + 1);
-                    // showBar();
-                    }}>Chocolate</button>
-                <button onClick={() => setVanillaVotes(vanillaVotes + 1)}>Vanilla</button>
-                <button onClick={() => setStrawberryVotes(strawberryVotes + 1)}>Strawberry</button>
-            </div>
-            { getTotal() === 0 ?
-            <>
-            <p>No votes yet</p>
-            </>
-            :
-            <>
-          {chocolateVotes > 0 ?
-            <p>
+      {getTotal() === 0 ? (
+        <>
+          <p>No votes yet</p>
+        </>
+      ) : (
+        <>
+          {chocolateVotes > 0 ? (
+            <div>
+              <p>
                 <label>Chocolate</label> {chocolateVotes} ({calculatePercentage(chocolateVotes, getTotal())})
-            </p>
-            <progress className="progressChocolate" value={ chocolateVotes / getTotal()}></progress>
-        :  <div></div>
-        }
-            <p>
+              </p>
+              <progress className="progressChocolate" value={chocolateVotes / getTotal()}></progress>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {vanillaVotes > 0 ? (
+            <div>
+              <p>
                 <label>Vanilla</label> {vanillaVotes} ({calculatePercentage(vanillaVotes, getTotal())})
-            </p>
-            <progress className="progressVanilla" value={ vanillaVotes / getTotal()}></progress>
-            <p>
+              </p>
+              <progress className="progressVanilla" value={vanillaVotes / getTotal()}></progress>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {strawberryVotes > 0 ? (
+            <div>
+              <p>
                 <label>Strawberry</label> {strawberryVotes} ({calculatePercentage(strawberryVotes, getTotal())})
-            </p>
-            <progress className="progressStrawberry" value={ strawberryVotes / getTotal()}></progress>
-            </>}
-        </div>
-    )
+              </p>
+              <progress className="progressStrawberry" value={strawberryVotes / getTotal()}
+              ></progress>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Votes;
