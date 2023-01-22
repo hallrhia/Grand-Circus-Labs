7a488bd95ab0f88c51cbee86a7b25ab024be7961
Rhianon Hall <hallrhia@gmail.com>
2023-01-22 14:29:04 -0500

    still fixing button onclick events

--- Ice-Cream-Wars/icecreamwars/src/components/Votes.tsx
+++ Ice-Cream-Wars/icecreamwars/src/components/Votes.tsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
 import '../css/Votes.css';
 
 function Votes() {
+    const [chocolateVotes, setChocolateVotes] = useState(0);
+    const [vanillaVotes, setVanillaVotes] = useState(0);
+    const [strawberryVotes, setStrawberryVotes] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+
+    function getTotal(){
+        return chocolateVotes + strawberryVotes + vanillaVotes;
+    }
+    
+    function calculatePercentage(votes:number, totalVotes:number){
+        return (votes / totalVotes * 100).toFixed(1) + '%';
+    }
+  
+    
+
     return(
-        <div>
-            
+        <div className="Votes">
+            <h2>Vote Here</h2>
+            <div className="button-bar">
+                <button onClick={()=> setChocolateVotes(chocolateVotes + 1)}>Chocolate</button>
+                <button onClick={() => setVanillaVotes(vanillaVotes + 1)}>Vanilla</button>
+                <button onClick={() => setStrawberryVotes(strawberryVotes + 1)}>Strawberry</button>
+            </div>
+            { getTotal() === 0 ?
+            <>
+            <p>No votes yet</p>
+            </>
+            :
+            <>
+            <p>
+                <label>Chocolate</label> {chocolateVotes} ({calculatePercentage(chocolateVotes, getTotal())})
+            </p>
+            <progress className="progressChocolate" value={ chocolateVotes / getTotal()}></progress>
+            <p>
+                <label>Vanilla</label> {vanillaVotes} ({calculatePercentage(vanillaVotes, getTotal())})
+            </p>
+            <progress className="progressVanilla" value={ vanillaVotes / getTotal()}></progress>
+            <p>
+                <label>Strawberry</label> {strawberryVotes} ({calculatePercentage(strawberryVotes, getTotal())})
+            </p>
+            <progress className="progressStrawberry" value={ strawberryVotes / getTotal()}></progress>
+            </>}
         </div>
     )
 }
